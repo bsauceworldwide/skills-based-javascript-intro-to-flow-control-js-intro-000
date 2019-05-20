c7b25e13876968294f3425d2af1d63b5facc8831
brad.schmalz <brad.schmalz@gmail.com>
2019-05-20 23:53:02 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -11,6 +11,12 @@ function teenager(age) {
 }
 
 function ageChecker(age) {
+  if (age >=13 && <=19) {
+    return "You are a teenager!";
+  } else if (age <13) {
+    return "You are a kid";
+  } else {return "You are a grownup";
+}
 
 }
 
